a79455f85767954fad8c296f1f8f844785b1cf00
Sahil Kumar <xdsahil@gmail.com>
2019-03-15 19:31:28 +0530

    Add MyOrder Screen
    Signed-off-by: Sahil Kumar <xdsahil@gmail.com>

--- lib/pages/myorders/order_item.dart
+++ lib/pages/myorders/order_item.dart
@@ -8,8 +8,103 @@ class OrderItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text(order.productDetails),
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 8.0),
+      child: Container(
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.black12),
+        ),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                mainAxisSize: MainAxisSize.max,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    'Order Id : ' + order.orderId.toString(),
+                    style: TextStyle(
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                  Text(
+                    order.productDetails,
+                    style: TextStyle(
+                      fontSize: 22.0,
+                      fontWeight: FontWeight.w500,
+                    ),
+                    maxLines: 1,
+                    softWrap: true,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  Text(
+                    'Order Placed : ' + order.orderDate,
+                    style: TextStyle(
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                  Row(
+                    children: <Widget>[
+                      Text(
+                        'Current Status : ',
+                        style: TextStyle(
+                          fontWeight: FontWeight.w500,
+                          fontSize: 18.0,
+                        ),
+                      ),
+                      Text(
+                        order.orderStatus,
+                        style: TextStyle(
+                          color: getStatusColor(order.orderStatus),
+                          fontWeight: FontWeight.w500,
+                          fontSize: 18.0,
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+            Container(
+              decoration: BoxDecoration(
+                  border: Border(top: BorderSide(color: Colors.black12))),
+              height: 48.0,
+              child: FlatButton(
+                shape: new RoundedRectangleBorder(
+                    borderRadius: new BorderRadius.circular(0.0)),
+                onPressed: () {},
+                child: Center(
+                  child: Text(
+                    "VIEW DETAILS",
+                    style: TextStyle(color: Colors.grey),
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
+}
+
+Color getStatusColor(String orderStatus) {
+  if (orderStatus == 'Processing')
+    return Colors.blueAccent;
+  else if (orderStatus == 'Confirmed')
+    return Colors.amber;
+  else if (orderStatus == 'Prepared')
+    return Colors.blueGrey;
+  else if (orderStatus == 'Dispatched')
+    return Colors.deepOrange;
+  else if (orderStatus == 'Delivered')
+    return Colors.green;
+  else if (orderStatus == 'Failed')
+    return Colors.red;
+  else
+    return Colors.black;
 }
