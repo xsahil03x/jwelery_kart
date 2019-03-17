25ad397fbf5bec91dd26c721573714917485d4ed
Sahil Kumar <xdsahil@gmail.com>
2019-03-18 00:13:21 +0530

    Wrap with Theme to use different primaryColor.
    Signed-off-by: Sahil Kumar <xdsahil@gmail.com>

--- lib/pages/registration/registration_screen.dart
+++ lib/pages/registration/registration_screen.dart
@@ -37,22 +37,27 @@ class RegistrationScreenState extends State<RegistrationScreen> {
       key: _scaffoldKey,
       backgroundColor: Colors.grey[100],
       body: Center(
-        child: Container(
-          margin: const EdgeInsets.symmetric(horizontal: 48.0),
-          child: new Column(
-            mainAxisSize: MainAxisSize.min,
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
+        child: Theme(
+          data: Theme.of(context).copyWith(
+            primaryColor: Colors.blueAccent,
+          ),
+          child: Container(
+            margin: const EdgeInsets.symmetric(horizontal: 48.0),
+            child: new Column(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
 //                Image.asset(
 //                  'assets/images/logo.png',
 //                  height: 90.0,
 //                ),
-              new SizedBox(
-                height: 48.0,
-              ),
-              (!showNumberPage ? numberPage() : otpPage()),
-            ],
+                new SizedBox(
+                  height: 48.0,
+                ),
+                (!showNumberPage ? numberPage() : otpPage()),
+              ],
+            ),
           ),
         ),
       ),
--- lib/pages/registration/user_info_screen.dart
+++ lib/pages/registration/user_info_screen.dart
@@ -35,156 +35,161 @@ class UserDetailForm extends StatelessWidget {
           child: SingleChildScrollView(
             child: Container(
               margin: const EdgeInsets.symmetric(horizontal: 24.0),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: <Widget>[
-                  new SizedBox(
-                    height: 48.0,
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: TextFormField(
-                      controller: registrationBloc.nameController,
-                      decoration: InputDecoration(
-                        icon: Icon(Icons.account_box),
-                        hintText: "Name",
-                        border: OutlineInputBorder(),
-                        contentPadding: EdgeInsets.symmetric(
-                            horizontal: 12.0, vertical: 16.0),
-                      ),
-                      validator: (text) {
-                        if (text.trim().length == 0)
-                          return "Please provide a valid name.";
-                      },
+              child: Theme(
+                data: Theme.of(context).copyWith(
+                  primaryColor: Colors.blueAccent,
+                ),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    new SizedBox(
+                      height: 48.0,
                     ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: TextFormField(
-                      controller: registrationBloc.addressController,
-                      decoration: InputDecoration(
-                        icon: Icon(Icons.my_location),
-                        hintText: "Address",
-                        border: OutlineInputBorder(),
-                        contentPadding: EdgeInsets.symmetric(
-                            horizontal: 12.0, vertical: 16.0),
-                      ),
-                      validator: (text) {
-                        if (text.trim().length == 0)
-                          return "Please provide a valid address.";
-                      },
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: TextFormField(
-                      controller: registrationBloc.cityController,
-                      decoration: InputDecoration(
-                        icon: Icon(Icons.location_city),
-                        hintText: "City",
-                        border: OutlineInputBorder(),
-                        contentPadding: EdgeInsets.symmetric(
-                            horizontal: 12.0, vertical: 16.0),
-                      ),
-                      validator: (text) {
-                        if (text.trim().length == 0)
-                          return "Please provide a valid city.";
-                      },
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: TextFormField(
-                      controller: registrationBloc.zipController,
-                      keyboardType: TextInputType.phone,
-                      decoration: InputDecoration(
-                        icon: Icon(Icons.local_post_office),
-                        hintText: "Zip",
-                        border: OutlineInputBorder(),
-                        contentPadding: EdgeInsets.symmetric(
-                            horizontal: 12.0, vertical: 16.0),
-                      ),
-                      validator: (text) {
-                        if (text.trim().length == 0)
-                          return "Please provide a valid zip code.";
-                      },
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: TextFormField(
-                      controller: registrationBloc.emailController,
-                      keyboardType: TextInputType.emailAddress,
-                      decoration: InputDecoration(
-                        icon: Icon(Icons.alternate_email),
-                        hintText: "Email",
-                        border: OutlineInputBorder(),
-                        contentPadding: EdgeInsets.symmetric(
-                            horizontal: 12.0, vertical: 16.0),
-                      ),
-                      validator: (text) {
-                        if (text.trim().length == 0 ||
-                            !isValidEmail(text.trim()))
-                          return "Please provide a valid email address.";
-                      },
-                    ),
-                  ),
-                  new SizedBox(
-                    height: 24.0,
-                  ),
-                  new RaisedButton(
-                    elevation: 2.0,
-                    disabledTextColor: Colors.grey[700],
-                    disabledColor: Colors.grey[300],
-                    color: Colors.blueAccent,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(20.0),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: TextFormField(
+                        controller: registrationBloc.nameController,
+                        decoration: InputDecoration(
+                          icon: Icon(Icons.account_box),
+                          hintText: "Name",
+                          border: OutlineInputBorder(),
+                          contentPadding: EdgeInsets.symmetric(
+                              horizontal: 12.0, vertical: 16.0),
+                        ),
+                        validator: (text) {
+                          if (text.trim().length == 0)
+                            return "Please provide a valid name.";
+                        },
                       ),
                     ),
-                    textColor: Colors.white.withOpacity(0.9),
-                    onPressed: () async {
-                      DialogUtils.showProgressBar(
-                          context, "Registering User...");
-                      var response = await registrationBloc.onSubmit();
-                      if (response == 'Success') {
-                        Navigator.pop(context);
-                        await Future.delayed(
-                          Duration(milliseconds: 200),
-                        );
-                        SnackbarUtils.show(
-                            _scaffoldKey, 'Successfully Registered');
-                        await Future.delayed(
-                          Duration(milliseconds: 700),
-                        );
-                        prefsHelper.isLogin = true;
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: TextFormField(
+                        controller: registrationBloc.addressController,
+                        decoration: InputDecoration(
+                          icon: Icon(Icons.my_location),
+                          hintText: "Address",
+                          border: OutlineInputBorder(),
+                          contentPadding: EdgeInsets.symmetric(
+                              horizontal: 12.0, vertical: 16.0),
+                        ),
+                        validator: (text) {
+                          if (text.trim().length == 0)
+                            return "Please provide a valid address.";
+                        },
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: TextFormField(
+                        controller: registrationBloc.cityController,
+                        decoration: InputDecoration(
+                          icon: Icon(Icons.location_city),
+                          hintText: "City",
+                          border: OutlineInputBorder(),
+                          contentPadding: EdgeInsets.symmetric(
+                              horizontal: 12.0, vertical: 16.0),
+                        ),
+                        validator: (text) {
+                          if (text.trim().length == 0)
+                            return "Please provide a valid city.";
+                        },
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: TextFormField(
+                        controller: registrationBloc.zipController,
+                        keyboardType: TextInputType.phone,
+                        decoration: InputDecoration(
+                          icon: Icon(Icons.local_post_office),
+                          hintText: "Zip",
+                          border: OutlineInputBorder(),
+                          contentPadding: EdgeInsets.symmetric(
+                              horizontal: 12.0, vertical: 16.0),
+                        ),
+                        validator: (text) {
+                          if (text.trim().length == 0)
+                            return "Please provide a valid zip code.";
+                        },
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: TextFormField(
+                        controller: registrationBloc.emailController,
+                        keyboardType: TextInputType.emailAddress,
+                        decoration: InputDecoration(
+                          icon: Icon(Icons.alternate_email),
+                          hintText: "Email",
+                          border: OutlineInputBorder(),
+                          contentPadding: EdgeInsets.symmetric(
+                              horizontal: 12.0, vertical: 16.0),
+                        ),
+                        validator: (text) {
+                          if (text.trim().length == 0 ||
+                              !isValidEmail(text.trim()))
+                            return "Please provide a valid email address.";
+                        },
+                      ),
+                    ),
+                    new SizedBox(
+                      height: 24.0,
+                    ),
+                    new RaisedButton(
+                      elevation: 2.0,
+                      disabledTextColor: Colors.grey[700],
+                      disabledColor: Colors.grey[300],
+                      color: Colors.blueAccent,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(20.0),
+                        ),
+                      ),
+                      textColor: Colors.white.withOpacity(0.9),
+                      onPressed: () async {
+                        DialogUtils.showProgressBar(
+                            context, "Registering User...");
+                        var response = await registrationBloc.onSubmit();
+                        if (response == 'Success') {
+                          Navigator.pop(context);
+                          await Future.delayed(
+                            Duration(milliseconds: 200),
+                          );
+                          SnackbarUtils.show(
+                              _scaffoldKey, 'Successfully Registered');
+                          await Future.delayed(
+                            Duration(milliseconds: 700),
+                          );
+                          prefsHelper.isLogin = true;
 
-                        Application.router.navigateTo(context, Routes.main);
-                      } else if (response == 'Fail') {
-                        Navigator.pop(context);
-                        SnackbarUtils.show(
-                            _scaffoldKey, 'Something went wrong...');
-                      } else {
-                        Navigator.pop(context);
-                        SnackbarUtils.show(_scaffoldKey, 'Invalid Data...');
-                      }
-                    },
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(
-                          vertical: 10.0, horizontal: 4.0),
-                      child: Text(
-                        "Save Details",
-                        style: new TextStyle(
-                            fontSize: 16.0, fontWeight: FontWeight.w700),
+                          Application.router.navigateTo(context, Routes.main);
+                        } else if (response == 'Fail') {
+                          Navigator.pop(context);
+                          SnackbarUtils.show(
+                              _scaffoldKey, 'Something went wrong...');
+                        } else {
+                          Navigator.pop(context);
+                          SnackbarUtils.show(_scaffoldKey, 'Invalid Data...');
+                        }
+                      },
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 10.0, horizontal: 4.0),
+                        child: Text(
+                          "Save Details",
+                          style: new TextStyle(
+                              fontSize: 16.0, fontWeight: FontWeight.w700),
+                        ),
                       ),
                     ),
-                  ),
-                  new SizedBox(
-                    height: 16.0,
-                  ),
-                ],
+                    new SizedBox(
+                      height: 16.0,
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
